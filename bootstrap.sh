afc2a2f0fb4b0fc06310cd7e57a4e498c88d127d
Christopher Goddard <chrisjgoddard@gmail.com>
2020-12-15 21:34:39 +1100

    Delete things that are not being used

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# curl -O -L https://raw.githubusercontent.com/token-cjg/hello_faas_cli/master/nginx.sh
-# curl -O -L https://raw.githubusercontent.com/token-cjg/hello_faas_cli/master/python.sh
-# curl -O -L https://raw.githubusercontent.com/token-cjg/hello_faas_cli/master/user.sh
-#
-# sudo chmod +x ./nginx.sh
-# sudo chmod +x ./user.sh
-# sudo chmod +x ./python.sh
-#
-# bash nginx.sh
-# bash user.sh
-# bash python.sh
-
 curl -O -L https://raw.githubusercontent.com/token-cjg/hello_faas_cli/master/docker.sh
 sudo chmod +x ./docker.sh
 bash docker.sh
