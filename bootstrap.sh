37b2743635dad45596187fb64e33d02541ee323e
Christopher Goddard <chrisjgoddard@gmail.com>
2020-12-15 21:33:36 +1100

    Organise code

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,40 +12,12 @@
 # bash user.sh
 # bash python.sh
 
-sudo apt-get update
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common -y
+curl -O -L https://raw.githubusercontent.com/token-cjg/hello_faas_cli/master/docker.sh
+sudo chmod +x ./docker.sh
+bash docker.sh
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# install openFaaS cli
+curl -sSL https://cli.openfaas.com | sudo -E sh
 
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-
-sudo usermod -aG docker $USER
-
-#Log out and log back in so that your group membership is re-evaluated.
-# If testing on a virtual machine, it may be necessary to restart the virtual machine for changes to take effect.
-# On a desktop Linux environment such as X Windows, log out of your session completely and then log back in.
-# On Linux, you can also run the following command to activate the changes to groups:
-newgrp docker
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-ssh-keyscan github.com >> ~/.ssh/known_hosts
-
-wget https://github.com/ivanpanshin/flask_gunicorn_nginx_docker/archive/master.zip
-sudo apt-get install unzip
-
-cd flask_gunicorn_nginx_docker-master
-bash run_docker.sh
+echo "You now need to manually set OPENFAAS_URL and then faas-cli login with the
+username and password of your faasd server or your throwaway server"
---
+++ docker.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+sudo usermod -aG docker $USER
+
+#Log out and log back in so that your group membership is re-evaluated.
+# If testing on a virtual machine, it may be necessary to restart the virtual machine for changes to take effect.
+# On a desktop Linux environment such as X Windows, log out of your session completely and then log back in.
+# On Linux, you can also run the following command to activate the changes to groups:
+newgrp docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+wget https://github.com/ivanpanshin/flask_gunicorn_nginx_docker/archive/master.zip
+sudo apt-get install unzip
+
+cd flask_gunicorn_nginx_docker-master
+bash run_docker.sh
