68cea2263134d7cf62524c7a94fbcba00e789bb1
Christopher Goddard <chrisjgoddard@gmail.com>
2020-12-15 11:17:15 +1100

    Update configuration a bit

--- fixtures/flaskapp/app/main.py
+++ fixtures/flaskapp/app/main.py
@@ -8,3 +8,6 @@ app = Flask(__name__)
 @app.route("/")
 def home_view():
 	return "<h1>Welcome to Geeks for Geeks</h1>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
--- python.sh
+++ python.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools -y
-sudo apt install python3-venv -y
+sudo apt install python3-venv python3.8 pipenv -y
 
 
 # get the Flask app
@@ -8,22 +8,36 @@ mkdir faas-cli-flask
 # Move into the parent directory for our Flask project.
 cd ~/faas-cli-flask
 mkdir app
-curl -O -L https://raw.githubusercontent.com/token-cjg/hello_faas_cli/master/fixtures/app/main.py
-curl -O -L https://raw.githubusercontent.com/token-cjg/hello_faas_cli/master/fixtures/wsgi.py
+curl -O -L https://raw.githubusercontent.com/token-cjg/hello_faas_cli/master/fixtures/flaskapp/app/main.py
+curl -O -L https://raw.githubusercontent.com/token-cjg/hello_faas_cli/master/fixtures/flaskapp/wsgi.py
 mv main.py app/main.py
 
 # Create a virtual environment to store your Flask project’s Python requirements by typing:
-python3.6 -m venv myprojectenv
-# This will install a local copy of Python and pip into a directory called myprojectenv within your project directory.
-
-# Before installing applications within the virtual environment, you need to activate it. Do so by typing:
-source myprojectenv/bin/activate
-
-# First, let’s install wheel with the local instance of pip to ensure that our packages will install even if they are missing wheel archives:
-pip install wheel
-
-# Next, let’s install Flask and Gunicorn:
-pip install gunicorn flask
+pipenv install wheel gunicorn flask
 
 # If you followed the initial server setup guide, you should have a UFW firewall enabled. To test the application, you need to allow access to port 5000:
 sudo ufw allow 5000
+
+# Set up a service to run this thing
+VENV_PATH=$(pipenv --venv)
+
+curl -O -L https://raw.githubusercontent.com/token-cjg/hello_faas_cli/master/fixtures/faas_cli_flask.service
+sed -i 's/replaceme/'"$VENV_PATH"'/g' faas_cli_flask.service
+mv faas_cli_flask.service /etc/systemd/system/faas_cli_flask.service
+
+# run the service
+sudo systemctl start faas_cli_flask
+sudo systemctl enable faas_cli_flask
+
+# route nginx to the service
+curl -O -L https://raw.githubusercontent.com/token-cjg/hello_faas_cli/master/fixtures/flaskapp/nginx/faas_cli_flask
+mv faas_cli_flask /etc/nginx/sites-available/faas_cli_flask
+
+# activate the nginx route
+sudo ln -s /etc/nginx/sites-available/faas_cli_flask /etc/nginx/sites-enabled
+
+# restart nginx to pick up the change
+sudo systemctl restart nginx
+
+sudo ufw delete allow 5000
+sudo ufw allow 'Nginx Full'
